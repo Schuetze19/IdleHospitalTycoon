cb09709df6cff8cad5434b8cdb696071a8aa8b56
Bonziller <bongziller@hotmail.de>
2018-03-14 22:29:07 +0100

    Zeile vergessen, wo die View ebenfalls verwendet wird.

--- app/src/main/java/de/idlepolicetycoon/project/view/MainActivity.java
+++ app/src/main/java/de/idlepolicetycoon/project/view/MainActivity.java
@@ -18,9 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        upgradeDialogController = new UpgradeDialogController(this.getWindow().getDecorView(), this);
         final View view = this.getWindow().getDecorView();
+        upgradeDialogController = new UpgradeDialogController(view, this);
         loadBackgroundImage(view,R.id.fahrstrecke_background,R.drawable.strassebsp);
         loadBackgroundImage(view,R.id.warteraum_background,R.drawable.warteraum);
         loadBackgroundImage(view,R.id.spielflaeche_background,R.drawable.hospital);
