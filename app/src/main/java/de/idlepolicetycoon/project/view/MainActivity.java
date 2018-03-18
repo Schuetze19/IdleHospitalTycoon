3f09806a2eb3499fd986b61b06442f412c36ecca
Bonziller <bongziller@hotmail.de>
2018-03-18 18:30:52 +0100

    Die App wird nun in "Fullscreen" angezeigt.

--- app/src/main/java/de/idlepolicetycoon/project/view/MainActivity.java
+++ app/src/main/java/de/idlepolicetycoon/project/view/MainActivity.java
@@ -3,6 +3,8 @@ package de.idlepolicetycoon.project.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.example.dennis.idlepolicetycoon.R;
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         final View view = this.getWindow().getDecorView();
         upgradeDialogController = new UpgradeDialogController(view, this);
